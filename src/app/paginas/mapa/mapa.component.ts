54ac13e60589f40ce709f2f387d246666da65e0a
juan.valencia@sistemgroup.es <juan.valencia@sistemgroup.es>
2021-02-23 16:10:50 +0100

    update

--- src/app/paginas/mapa/mapa.component.ts
+++ src/app/paginas/mapa/mapa.component.ts
@@ -12,56 +12,27 @@ export class MapaComponent implements OnInit
 
   ngOnInit(): void
   {
-    console.log('Ancho: ' + window.innerWidth);
-    console.log("Alto: " + window.innerHeight);
-    this.ir(800, 600);
 
   }
 
   posicion(evt: any)
   {
     const { currentTarget: svg, pageX, pageY } = evt;
-    console.log(pageX);
-    console.log(pageY);
   }
 
-  ir(x: any, y: any)
+  ir(y: any, x: any)
   {
-    // console.log('ir: ' + x + ' - ' + y);
-    console.log('Top: ' + (screen.height) );
-    console.log('Left: ' + (screen.width));
-    var ancho = screen.width;
-    var alto = screen.height;
-    // console.log('Top: ' + (x + 50));
-    // console.log('Left: ' + (y));
-    //     window.scroll({
-    //   top: 800,
-    //   left: 600,
-    //   behavior: 'smooth'
-    // });
-    var coordX = -(240/1.5);
-    var coordY = -(121/1.5) ;
+    var centroX = screen.height / 2;
+    var centroY = screen.width / 2;
+    var coordX = centroX - x - 50;
+    var coordY = centroY - y;
     var elemento = document.getElementById('mapaImg');
     if (elemento != null)
     {
       elemento.classList.remove("normal");
-      // elemento.classList.add("izquierda");
-      elemento.style.top=coordX + "px";
-      elemento.style.left=coordY + "px";
+      elemento.style.top = coordX + "px";
+      elemento.style.left = coordY + "px";
     }
-
-
-    console.log('Top: ' +coordX);
-    console.log('Left: ' + coordY);
-    // if (elemento != null)
-    // {
-    //   elemento.classList.add("zoom");
-    // }
-    // window.scroll({
-    //   top: 600,
-    //   left: 300,
-    //   behavior: 'smooth'
-    // });
 
   }
 }
